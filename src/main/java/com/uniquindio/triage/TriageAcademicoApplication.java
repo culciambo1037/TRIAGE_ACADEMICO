3360c72dd373b59071739e88273d529f08aa8acd
culciambo1037 <cagiraldov@uqvirtual.edu.co>
2026-03-11 19:30:36 -0500

    Inicializar proyecto Spring Boot - Hito 2 Triage Académico

---
+++ src/main/java/com/uniquindio/triage/TriageAcademicoApplication.java
@@ -0,0 +1,13 @@
+package com.uniquindio.triage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TriageAcademicoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TriageAcademicoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/uniquindio/triage/TriageAcademicoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uniquindio.triage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TriageAcademicoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
